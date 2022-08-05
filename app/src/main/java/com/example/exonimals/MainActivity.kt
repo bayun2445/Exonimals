237500d41c1ca4ff9e70cddbfd8c468a07ebe429
bayun2445 <bayun2445@gmail.com>
2022-08-05 22:23:46 +0700

    added codes to show recycler view in Main Activity

--- app/src/main/java/com/example/exonimals/MainActivity.kt
+++ app/src/main/java/com/example/exonimals/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.exonimals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var rvAnimals: RecyclerView = findViewById(R.id.rv_animals)
+    private var list = AnimalData.listAnimals
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_animals.setHasFixedSize(true)
+        showListAnimal()
+    }
+
+    private fun showListAnimal() {
+        val listAnimalAdapter = ListAnimalAdapter(list)
+        rv_animals.layoutManager = LinearLayoutManager(this)
+        rv_animals.adapter = listAnimalAdapter
     }
 }
